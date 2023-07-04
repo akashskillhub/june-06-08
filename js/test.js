baaee21249b644294be67cf0c81c9eaec3015ff2
akash <akash@gmail.com>
2023-07-04 12:45:33 +0530

    03-07-2023

--- js/test.js
+++ js/test.js
@@ -1,6 +1,14 @@
-const obj = { brand: "dell" }
-// const x = obj  // Not a Copy
-const x = { ...obj } // shalow copy
-x.brand = "apple"
+const cart = [
+    { product: "keyboard", price: 500, qty: 2 },
+    { product: "mouse", price: 200, qty: 1 }
+]
 
-console.log(obj)
+let total = 0
+for (let i = 0; i < cart.length; i++) {
+    total += cart[i].price * cart[i].qty
+}
+// for (const item of cart) {
+//     total += item.price * item.qty
+// }
+console.log(total)
+// 700
